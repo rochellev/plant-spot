432ad1057dd38440f459f7cccceb11327cef7d0f
rochellev <rochellevroberts@utexas.edu>
2021-03-03 15:33:28 -0800

    add reducer to get a static list of plants, and make use of combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,22 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from "redux";
 
+const plantListReducer = () => {
+  return [
+    { id: 1, common_name: "Daisy" },
+    { id: 2, common_name: "Orchid" },
+    { id: 3, common_name: "Lemon Tree" },
+    { id: 4, common_name: "Big Tree" }
+  ];
+};
 const selectedPlantIdReducer = (selectedPlantId = null, action) => {
-  if(action.type === 'PLANT_ID_SELECTED'){
-    return action.payload
+  if (action.type === "PLANT_ID_SELECTED") {
+    return action.payload;
   }
   return selectedPlantId;
-}
+};
+
+
+export default combineReducers({
+    plantList: plantListReducer,
+    selectedPlantId: selectedPlantIdReducer
+})
