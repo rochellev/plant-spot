2eb99227c926bcfa81c9eb486fbbfb7fd003d11c
rochellev <rochellevroberts@utexas.edu>
2021-03-09 11:39:26 -0800

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,9 @@
+// import jsonPlaceholder from '../api/jsonPlaceholder';
+
+// export const fetchPosts = async () => {
+
+// }
+
 export const selectPlantId = plantId => {
   return {
     type: 'PLANT_ID_SELECTED',
